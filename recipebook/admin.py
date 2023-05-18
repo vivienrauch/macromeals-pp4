c1454920c8c32d320c23dd67a40da207d011493b
vivienrauch <vivienmirjam@gmail.com>
2023-05-18 20:11:37 +0000

    Add entry and comment classes and details to models.py

--- recipebook/admin.py
+++ recipebook/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Entry
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Entry)
+class EntryAdmin(SummernoteModelAdmin):
+    
+    summernote_fields = ('content')
