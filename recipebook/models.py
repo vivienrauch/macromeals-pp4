f11ab1ef0b1bb15f6e9694a58511d368a18f70b7
vivienrauch <vivienmirjam@gmail.com>
2023-05-18 09:56:48 +0000

    Add basics of the entry class to models.py

--- recipebook/models.py
+++ recipebook/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Entry(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique = True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
