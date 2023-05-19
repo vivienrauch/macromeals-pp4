827a85e4c53063e4f6965fecb1df5d85304667bd
vivienrauch <vivienmirjam@gmail.com>
2023-05-19 08:13:07 +0000

    add macros custom models

--- recipebook/admin.py
+++ recipebook/admin.py
@@ -6,4 +6,8 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Entry)
 class EntryAdmin(SummernoteModelAdmin):
     
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ('title', 'content', 'protein', 'carbs', 'fat')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
