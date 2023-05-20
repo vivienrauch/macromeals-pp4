53ab07df4cbef742db10c3d33a8e239d6d8201a7
vivienrauch <vivienmirjam@gmail.com>
2023-05-20 11:22:36 +0000

    Add bootstrap template

--- recipebook/views.py
+++ recipebook/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Entry
 
-# Create your views here.
+
+class EntryList(generic.ListView):
+    model = Entry
+    queryset = Post.objects.filter(status=1).order_by('-created-_on')
+    template_name = 'index.html'
+    paginate_by = 6
