519f3f79f3b1fc0a94ee49f228519a316b82ba53
vivienrauch <vivienmirjam@gmail.com>
2023-06-04 11:03:32 +0000

    Add comment form functionality

--- recipebook/forms.py
+++ recipebook/forms.py
@@ -6,3 +6,13 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = (
+            'name',
+            'email',
+            'body',
+        )
--- recipebook/views.py
+++ recipebook/views.py
@@ -14,6 +14,7 @@ class EntryList(generic.ListView):
 class EntryDetail(View):
 
     def get(self, request, slug, *args, **kwargs):
+
         queryset = Entry.objects.filter(status=1)
         entry = get_object_or_404(queryset, slug=slug)
         comments = entry.comments.filter(approved=True).order_by('created_on')
@@ -25,7 +26,40 @@ class EntryDetail(View):
             {
                 "entry": entry,
                 "comments": comments,
+                "commented": False,
                 "rating": rating,
                 "comment_form": CommentForm()
             },
         )
+
+    def post(self, request, slug, *args, **kwargs):
+
+        queryset = Entry.objects.filter(status=1)
+        entry = get_object_or_404(queryset, slug=slug)
+        comments = entry.comments.filter(approved=True).order_by('created_on')
+        rating = entry.average_rating
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.entry = entry
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "entry_detail.html",
+            {
+                "entry": entry,
+                "comments": comments,
+                "commented": True,
+                "rating": rating,
+                "comment_form": comment_form
+            },
+        )
+
+
