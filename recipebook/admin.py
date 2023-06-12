04c1c387e9b5b38a3dd93d43a57a11502755ee82
vivienrauch <vivienmirjam@gmail.com>
2023-06-12 13:09:04 +0000

    Add contact to admin py

--- recipebook/admin.py
+++ recipebook/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Entry, Comment, Rating
+from .models import Entry, Comment, Rating, Contact
 from django_summernote.admin import SummernoteModelAdmin
 
 
